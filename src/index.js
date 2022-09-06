7efa260f37bdfeb4f6555bb0b66d7e2acd836904
Christian <christianyip92@gmail.com>
2022-09-06 11:54:11 -0700

    Add parameter for weather unit.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 console.log('texting index.js');
 const weatherAPI = '904aa33c255754744867c717f0deff45';
+const fahrenheit = '&units=imperial';
+const celsius = '&units=metric';
 
-const getCurrentWeather = async () => {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${weatherAPI}`);
+const getCurrentWeather = async (unit) => {
+  const unitSelection = unit;
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99${unitSelection}&appid=${weatherAPI}`);
   const currentWeatherData = await response.json();
   console.log(currentWeatherData);
+  console.log(currentWeatherData.main.temp);
 };
 
-getCurrentWeather();
+getCurrentWeather(fahrenheit);
