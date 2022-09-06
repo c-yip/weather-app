34160efff0211a347c020987e40e0a0fb71e6b90
Christian <christianyip92@gmail.com>
2022-09-06 13:15:19 -0700

    Add search by city feature.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-console.log('texting index.js');
 const weatherAPI = '904aa33c255754744867c717f0deff45';
-const fahrenheit = '&units=imperial';
-const celsius = '&units=metric';
+const locationSearch = document.getElementById('search');
+const submit = document.getElementById('submit');
+const fahrenheit = 'imperial';
+const celsius = 'metric';
+let location;
 
-const getCurrentWeather = async (unit) => {
+submit.addEventListener('click', () => {
+  location = locationSearch.value;
+  console.log(location);
+  getCurrentWeather(fahrenheit, location)
+    .then((currentWeatherData) => console.log(currentWeatherData));
+});
+
+// fetches weather data
+const getCurrentWeather = async (unit, location) => {
   const unitSelection = unit;
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99${unitSelection}&appid=${weatherAPI}`);
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unitSelection}&appid=${weatherAPI}`);
   const currentWeatherData = await response.json();
-  console.log(currentWeatherData);
-  console.log(currentWeatherData.main.temp);
+  return currentWeatherData;
 };
-
-getCurrentWeather(fahrenheit);
