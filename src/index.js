9474b483ca8b2e1215ced8c537dda3a900ec8c07
Christian <christianyip92@gmail.com>
2022-09-06 14:50:23 -0700

    Add variables for data object properties.

--- src/index.js
+++ src/index.js
@@ -9,15 +9,33 @@ submit.addEventListener('click', () => {
   location = locationSearch.value;
   console.log(location);
   getCurrentWeather(fahrenheit, location)
-    .then((currentWeatherData) => console.log(currentWeatherData))
+    .then((currentWeatherData) => console.log(currentWeatherData.description))
     .catch((err) => console.log('Error:', err.message));
 });
 
 // fetches weather data
-const getCurrentWeather = async (unit, location) => {
+const getCurrentWeather = async (unit, loc) => {
   const unitSelection = unit;
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unitSelection}&appid=${weatherAPI}`);
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&units=${unitSelection}&appid=${weatherAPI}`);
 
-  const currentWeatherData = await response.json();
+  const data = await response.json();
+
+  console.log(data);
+
+  const currentWeatherData = {
+    currentTemp: data.main.temp,
+    locationName: data.name,
+    description: data.weather[0].description,
+    icon: data.weather[0].icon,
+    clouds: data.clouds.all,
+    wind: `${data.wind.gust} mph`,
+    feelsLike: data.main.feels_like,
+    humidity: data.main.humidity,
+    min: data.main.temp_min,
+    max: data.main.temp_max,
+  };
+
   return currentWeatherData;
 };
+
+getCurrentWeather(fahrenheit, 'west covina');
